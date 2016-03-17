09b577b93577a3dd6fdb3dc174ae3e54b93ccbf4
Jonas Solsvik <jonas.solsvik@gmail.com>
2016-03-17 12:42:45 +0100

    New function - points_calc()
    - Ingen ny funksjonalitet. Bare gammel logikk som er
       puttet i en egen funksjon.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,69 @@
+"""
+   Tittel: Sentence Crusher
+   Authors: Jonas, Glenn, Emil, JanCato
+   Startet: 16.03.2016
+
+"""
+
 import time
 
 
-def new_game():
+def points_calc(points, clock_before, string, user_string):
+    """ This function calculates the final score based on three
+         criteria:
+           - Time spent - more time less points
+           - Wrong letters - more wrong, less points
+           - Wrong length - more difference, less points
+
+    :param points: Starting value of points. Default 300.
+    :param clock_before: CPU-time just before the player started typing.
+    :param string: This is the text the user tried to match.
+    :param user_string: This is the text that is going to be evaluated
+                        against string.
+    :return: points - the final score is returned.
+    """
+    # --- Evaluate how much time the user has spent typing ---
+    # --- Penalty = - 10 points for each second after 5 seconds ---
+
+    clock_after = time.clock()   # checks time after input
+    clock_diff = clock_after - clock_before
+    if clock_diff <= 5.0:
+        pass
+    elif clock_diff > 5.0:
+        points -= (-50+(clock_diff*10))
+
+    points = int(points)
+    print("Clock_diff: %.2f sekund." % clock_diff)  
+    print("After clock_diff: ", points, "points")   # debug data
     
+    # --- Evaluate if each letter is correct ----
+    # --- Penalty 2 points for each letter wrong----
+    wrong_letter = 0
+    for i in range(len(string)):
+        if string[i] == user_string[i]:
+            pass
+        else:
+            wrong_letter += 1
+            points -= 2
+    print("Wrong letters: ", wrong_letter)
+    print("After letter check: ", points, "points")    # debug data
+
+    # --- Evaluate difference in length -------
+    # --- Penalty 20 points for each letter difference ---
+    length_diff = abs(len(string) - len(user_string))
+    points -= (length_diff*20)
+    print("Length_diff:  ", length_diff)
+    print("After length_diff: ", points, "points")   # debug data
+
+    return points
+
+
+def new_game():
     # --------------------  INIT -------------------- #
     string = "Lisa gikk til skolen. Tripp, tripp, tripp det sa. I den nye kjolen, trippet hun så glad."
     points = 300
 
     # --------------------  GRAPHICS -------------------- #
-    # --- Show string ---
     input("Tast enter, og gjør deg klar til å skrive!")
     for i in (3, 2, 1):
         print(i)
@@ -26,58 +81,24 @@ def new_game():
           -------------------------------------- Jonas --------   """
     print(string)
     user_string = input()
-    
+
     # --------------------- LOGIC ----------------------- # 
-    # --- Evaluate how much time the user has spent typing ---
-    # --- Penalty = - 10 points for each second after 5 seconds ---
-
-    clock_after = time.clock()   # checks time after input
-    clock_diff = clock_after - clock_before
-    if clock_diff <= 5.0:
-        pass
-    elif clock_diff > 5.0:
-        points -= (-50+(clock_diff*10))
-
-    points = int(points)
-    print("Clock_diff: %.2f sekund." % clock_diff)  
-    print("After clock_diff: ", points, "points")   # debug data
-    
-    # --- Evaluate if each letter is correct ----
-    # --- Penalty 20 points for each letter wrong----
-    wrong_letter = 0
-    for i in range(len(string)):
-        if string[i] == user_string[i]:
-            pass
-        else:
-            wrong_letter += 1
-            points -= 2
-    print("Wrong letters: ", wrong_letter)
-    print("After letter check: ", points, "points")    # debug data
-
-    # --- Evaluate difference in length -------
-    # --- Penalty 10 points for each letter difference ---
-    length_diff = abs(len(string) - len(user_string))
-    points -= (length_diff*10)
-    print("Length_diff:  ", length_diff)
-    print("After length_diff: ", points, "points")   # debug data
+    points = points_calc(points, clock_before, string, user_string)
 
     # --------------------  GRAPHICS -------------------- #
-    # --- Show total points -------
     print("Total points: ", int(points), "points")
     
     # --------------------  INPUT -------------------- #
-    # --- Start again ----
     yes_or_no = input("Start again? Yes?")
     if yes_or_no.upper() == "YES":
         new_game()
 
 
 if __name__ == "__main__":
-    # Here we will insert functions that will initialize the game.
     new_game()
 
 
-
 """
+  session 12:25  - 12:40        Jonas
   session 00:25  - 01:31        Jonas
 """
