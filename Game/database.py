53edb044576670a1fc5d1790b0cd038427ad3f21
Jonas Solsvik <jonas.solsvik@gmail.com>
2016-03-21 13:20:44 +0100

    Added comments to database.py

--- Game/database.py
+++ Game/database.py
@@ -5,11 +5,10 @@ def read_into_dict(level):
       Function will return a dictionary. 
        The dictionary will contain the entire high-score
         list for the current level like this: 
-         { Name1: [Points, Time-stamp, Duration],
-           Name2: [Points, Time-stamp, Duration] }
+         { Name1: [Points, Time-stamp, Duration, level, game],
+           Name2: [Points, Time-stamp, Duration, level, game] }
            ....
-           ----------------------------- Jonas ----
-    """
+           ----------------------------- Jonas ----      """
     file = open('Highscorelists\level{0}.txt'.format(level), 'r')
     
     highscore_dict = {} 
@@ -32,10 +31,9 @@ def write_from_dict(level, highscore_dict):
     """
       Function writes from the modified dictionary,
        back to the file in this format: 
-        name, points, Time-stamp, Duration, level, game\n
+        name,points,Time-stamp,Duration,level,game\n
         -------------------------- Jonas ----
        """
-
     file = open('Highscorelists\level{0}.txt'.format(level), 'w')
     while True: 
         try:
@@ -55,7 +53,6 @@ def write_from_dict(level, highscore_dict):
             break
 
     file.close()
-
     return 0
 
 
@@ -68,11 +65,13 @@ def store_data(new_data):
     - level_int   index [4]
     - Game_name   index [5]
     ------------ Jonas ---"""
-
     level = new_data[4]     #Gets a integer between 1-4
-    
+
+    # read_into_dict() creates a dictionary from file-data
     highscore_dict = read_into_dict(level)
+    # dictionary is modified with new data
     highscore_dict[new_data[0]] = [new_data[1:]]
+    # The modified dictionary is written back to the file.
     write_from_dict(level, highscore_dict)
 
 
