78e8f2088d4987909e8b7ff2b27dd23a3584e2d2
Jonas Solsvik <jonas.solsvik@gmail.com>
2016-04-04 00:58:55 +0200

    Implemented pickle to read/write objects to .pickle-files.
    This replaces the .txt - file system we had before.

--- Source/Server/storage.py
+++ Source/Server/storage.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import os
+import pickle
 
 class StorageGuy:
     """ This class reads from, and writes to files, and sends data to server"""
@@ -15,7 +16,7 @@ class StorageGuy:
                 non-existent.
         ------------------------ Jonas ---"""
         # --- Get data.level_history ----
-        self.read_from_file(data)
+        data.level_history = self.read_from_file(data.level)
         # Task 1
         try: 
             old_points = int(data.level_history[data.user][0])
@@ -25,61 +26,35 @@ class StorageGuy:
         if int(data.points) > old_points:
             # Task 2    
             data.level_history[data.user] = data.new_data
-            self.write_to_file(data)
+            self.write_to_file(data.level_history, data.level)
         else: 
             pass
 
-    def read_from_file(self, data):
-        """ Function will return a dictionary. 
-            The dictionary will contain the entire 
-             dataset for the current level like this: 
-             { Name1: [Points, Time-stamp, Duration, level, game],
-               Name2: [Points, Time-stamp, Duration, level, game] }
-               ....
-               ----------------------------- Jonas ----      """
-        level = str(data.level)
-
-        # The next line is here to ensure file-path-compability on all operating systems
-        os.path.join(os.path.dirname(__file__),('Highscorelists/level{0}.txt'.format(level)))
-        file = open('Highscorelists/level{0}.txt'.format(level), 'r')
+    def read_from_file(self, level):
+        """ Load pickle data from level-file. """
+        path = self.make_path(level)
+        with open(path, 'rb') as file:
+              return pickle.load(file)      
         
-        while True:
-            raw_line = file.readline()               # Gets a string
-            line = raw_line.replace("\\n", "")       # Removes "\n" from string 
-            line_list = line.split(',')              # Makes string into list
-            
-            if line == "":                           # This means end of file
-                break
-            else:
-                #   DICTIONARY   [KEY]       = [VALUE1, VALUE2, VALUE3...]
-                data.level_history[line_list[0]] = line_list[1:]
+    def write_to_file(self, record, level):
+        """ Dump pickle data to level-file"""
+        path = self.make_path(level)
+        with open(path, 'wb') as file:
+            pickle.dump(record, file)
 
-        file.close()
-
-    def write_to_file(self, data):
-        """Function writes from level_history, 
-            which possibly contains new data,
-             back to the file in this format: 
-           'name,points,Time-stamp,Duration,level,game\n'
-            -------------------------- Jonas ---- """
-        level = str(data.level)
-
-        # The next line is here to ensure file-path-compability on all operating systems
-        os.path.join(os.path.dirname(__file__),('Highscorelists/level{0}.txt'.format(level)))
-        file = open('Highscorelists/level{0}.txt'.format(level), 'w')
-
-        updated_dict = data.level_history    
-        unwanted_characters = ['[',']', '\'', ' ']
+    def make_path(self, level):
+        return (os.path.join(os.path.dirname(__file__),('levelrecords/level{0}.pickle'.format(level))))
         
 
-        for key in updated_dict:   # key: is username:
-            not_write_this = ''
-            write_this = ''
-            not_write_this = str(key) + "," + str(updated_dict[key])
-            for char in not_write_this:
-                if char in unwanted_characters:
-                    pass
-                else:
-                    write_this += char
-            file.write(write_this + '\n')
-        file.close()
+# Debug area
+if __name__ == '__main__':
+
+    storage = StorageGuy()
+    # - The following three lines was run when the files where empty, 
+    # to fill them with some form of data. 
+    # dummy_dict = {'Bottomline': ['0', 'dummylevel']}
+    # for level in range(1, 5):
+        # storage.write_to_file(dummy_dict, level)
+
+    for level in range(1, 5):
+        print(storage.read_from_file(level))
